73dd3e9cd24e884476162a6c968c3da54c9a118e
Ben-Ammar1312 <ben.ammar1312@gmail.com>
2025-07-15 12:30:12 +0100

    test: authorize doctor controller

--- src/test/java/com/example/backend/controller/DoctorControllerTest.java
+++ src/test/java/com/example/backend/controller/DoctorControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -30,7 +32,8 @@ class DoctorControllerTest {
     void getPatients_returns200AndDelegatesToService() throws Exception {
         given(fertilityRecordService.getAllRecords()).willReturn(List.of(new FertilityRecord()));
 
-        mockMvc.perform(get("/doctor/patients"))
+        mockMvc.perform(get("/doctor/patients")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_Doctor"))))
                 .andExpect(status().isOk());
 
         verify(fertilityRecordService).getAllRecords();
@@ -41,7 +44,8 @@ class DoctorControllerTest {
         given(fertilityRecordService.getFullFertilityRecord("42"))
                 .willReturn(FertilityRecordDetails.builder().build());
 
-        mockMvc.perform(get("/doctor/patient/{id}", "42"))
+        mockMvc.perform(get("/doctor/patient/{id}", "42")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_Doctor"))))
                 .andExpect(status().isOk());
 
         verify(fertilityRecordService).getFullFertilityRecord("42");
