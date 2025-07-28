f88d7d1aa96e20d3eb04d7f92a828f30a730a2e4
Ben-Ammar1312 <ben.ammar1312@gmail.com>
2025-07-28 13:34:58 +0100

    test fix

--- src/main/java/com/example/backend/repository/FertilityRecordRepository.java
+++ src/main/java/com/example/backend/repository/FertilityRecordRepository.java
@@ -3,8 +3,12 @@ package com.example.backend.repository;
 import com.example.backend.model.FertilityRecord;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 
 public interface FertilityRecordRepository
         extends MongoRepository<FertilityRecord, String> {
+
+    List<FertilityRecord> findAllByMalePartner_PersonalInfo_FirstNameIsNotNull();
 
 }
